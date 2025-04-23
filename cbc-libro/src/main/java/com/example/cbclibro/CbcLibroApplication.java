f4006cce63b3f82108bbc19fc837cc0711170e10
carl0s8466 <138223317+carl0s8466@users.noreply.github.com>
2025-04-23 08:36:21 -0500

    Add files via upload

---
+++ cbc-libro/src/main/java/com/example/cbclibro/CbcLibroApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbclibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbcLibroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbcLibroApplication.class, args);
+    }
+
+}
